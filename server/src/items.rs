4069130e8b625a3e68d6f783a3277554e1168d51
Ravi Mistry <rvstry@protonmail.com>
2025-02-16 20:59:07 +0000

    get item by id

--- server/src/items.rs
+++ server/src/items.rs
@@ -4,14 +4,15 @@ use utoipa::ToSchema;
 use serde::Serialize;
 use axum::response::IntoResponse;
 use sqlx::FromRow;
+use axum::extract::Path;
 
 use crate::database::get_db;
 
 #[derive(ToSchema)]
 #[derive(Serialize)]
 #[derive(Clone, FromRow, Debug)]
-struct Item {
-    id: u64,
+pub struct Item {
+    id: u32,
     name: String,
     description: String,
     damages: String,
@@ -29,4 +30,31 @@ pub async fn get_items() -> Response {
     let pool = &get_db().await.pool;
     let results = sqlx::query_as::<_, Item>("SELECT * FROM items").fetch_all(pool).await.unwrap();
     Json(results).into_response()
+}
+
+/// Get item by id
+///
+/// Get item from database by item id
+#[utoipa::path(
+get,
+path = "/items/{id}",
+responses(
+    (status = 200, description = "Item found successfully", body = Item),
+    (status = NOT_FOUND, description = "Item was not found", body = ())
+),
+params(
+    ("id", description = "Item database id to get Item for"),
+)
+)]
+pub async fn get_item_by_id(Path(id): Path<u32>) -> Response  {
+    let pool = &get_db().await.pool;
+    println!("the item id {}",id);
+    let result2 = sqlx::query_as::<_, Item>("SELECT * FROM items WHERE id=?").bind(id).fetch_one(pool).await;
+
+    if result2.is_ok() {
+        return Json(result2.unwrap()).into_response();
+    }
+    else {
+        return Json(()).into_response();
+    }
 }
