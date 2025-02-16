d8296eb12a1e2ab8c37b246f0215c044e8f8648b
JackB <sr23140@bristol.ac.uk>
2025-02-16 19:07:43 +0000

    FIXED FOREIGN KEY REFERENCES HOLY

--- server/migrations/1_init.sql
+++ server/migrations/1_init.sql
@@ -15,15 +15,15 @@ CREATE TABLE IF NOT EXISTS tags
 
 CREATE TABLE IF NOT EXISTS item_tags
 (
-    tag_id      INTEGER REFERENCES tag(tag_id) ON DELETE CASCADE NOT NULL,
-  	item_id		INTEGER REFERENCES item(item_id) on DELETE CASCADE NOT NULL,
+    tag_id      INTEGER REFERENCES tag(id) ON DELETE CASCADE NOT NULL,
+  	item_id		INTEGER REFERENCES item(id) on DELETE CASCADE NOT NULL,
   	PRIMARY KEY (tag_id, item_id)
 );
 
 CREATE TABLE IF NOT EXISTS images
 (
     id    INTEGER PRIMARY KEY NOT NULL,
-    item_id     INTEGER REFERENCES item(item_id) on DELETE CASCADE NOT NULL,
+    item_id     INTEGER REFERENCES item(id) on DELETE CASCADE NOT NULL,
     description TEXT                NOT NULL,
     image_name  TEXT                NOT NULL
 );
@@ -36,8 +36,8 @@ CREATE TABLE IF NOT EXISTS users(
 
 CREATE TABLE IF NOT EXISTS borrows(
     id    INTEGER PRIMARY KEY NOT NULL,
-    item_id      INTEGER REFERENCES item(item_id) on DELETE CASCADE NOT NULL,
-    user_id      INTEGER REFERENCES user(user_id) on DELETE CASCADE NOT NULL,   
+    item_id      INTEGER REFERENCES item(id) on DELETE CASCADE NOT NULL,
+    user_id      INTEGER REFERENCES user(id) on DELETE CASCADE NOT NULL,   
     damage       BOOLEAN                NOT NULL DEFAULT 0,
     borrrow_date DATETIME               NOT NULL DEFAULT CURRENT_TIMESTAMP,
     due_date     DATETIME               NOT NULL DEFAULT CURRENT_TIMESTAMP,
